f552908333b97147ee8c87e19ff9455f7693c11c
itautomation withpankaj <itautomationwithpankaj@gmail.com>
2024-01-27 05:44:02 +0000

    init

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+   sno = db.Column(db.Integer, primary_key=True)
+   title = db.Column(db.String(200), nullable=False)
+   desc = db.Column(db.String(500), nullable=False)
+   date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+def __repr__(self) -> str:
+    return f"{self.sno} - {self.title}"
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+    #return 'Hello, World!'
 
-if __name__ == "main":
-  app.run(debug=True)
+@app.route('/products')
+def products():
+  return 'this is products!'
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=8000)
+
+# Creating a database
+# pip install flask-sqlalchemy
+
